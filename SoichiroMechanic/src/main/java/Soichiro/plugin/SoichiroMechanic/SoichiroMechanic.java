9d190022a26f259ba87bb8d12ed5f09b14c5866f
soichiro0520 <hanasou5555@gmail.com>
2025-12-06 13:19:01 +0000

    ちょっと変更

--- SoichiroMechanic/src/main/java/Soichiro/plugin/SoichiroMechanic/SoichiroMechanic.java
+++ SoichiroMechanic/src/main/java/Soichiro/plugin/SoichiroMechanic/SoichiroMechanic.java
@@ -1,20 +1,12 @@
 package Soichiro.plugin.SoichiroMechanic;
 
 import org.bukkit.plugin.java.JavaPlugin;
-import Soichiro.plugin.SoichiroMechanic.managers.PluginManager;
-import Soichiro.plugin.SoichiroMechanic.listeners.PlayerListener;
+
 
 public class SoichiroMechanic extends JavaPlugin {
     
     @Override
     public void onEnable() {
-        
-        // Initialize managers
-        PluginManager.getInstance().initialize();
-        
-        // Register listeners
-        getServer().getPluginManager().registerEvents(new PlayerListener(), this);
-        
         getLogger().info("SoichiroMechanic has been enabled!");
     }
 
